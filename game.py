fc053f81e38d82a0f53c932980a5708c8e3ebccd
Chase Fashing <chasefashing@gmail.com>
2022-06-21 20:41:49 -0500

    Lots of FISH swimming

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 import pygame
-
+import random
 
 #VARIABLES#
 window_size = (550,400)
 fishy_left_image = pygame.image.load('static/images/fishy_left.png')
 fishy_right_image = pygame.image.load('static/images/fishy_right.png')
 screen = pygame.display.set_mode(window_size)
+FPS = 30
+MAX_FISH = 4
+MAX_FISH_SPEED = 10
+MIN_FISH_SPEED = 3
 
 class Fishy:
   ##Init function
@@ -15,7 +19,7 @@ class Fishy:
     self.height = 8 
     self.x = 225
     self.y = 200
-    self.direction:char = 'R'
+    self.direction = 1
     self.x_speed = 0
     self.y_speed = 0
     self.max_x_speed = 8
@@ -52,10 +56,10 @@ class Fishy:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
       self.x_speed -= .25
-      self.direction = 'L'
+      self.direction = -1
     if keys[pygame.K_RIGHT]:
       self.x_speed += .25
-      self.direction = 'R'  
+      self.direction = 1  
     if keys[pygame.K_UP]:
         self.y_speed -= .25
     if keys[pygame.K_DOWN]:
@@ -68,7 +72,7 @@ class Fishy:
   ##Draw fishy image to screen
   def draw(self,screen):
     if self.alive:
-      screen.blit(self.image_left if self.direction == 'L' else self.image_right,(self.x,self.y))
+      screen.blit(self.image_left if self.direction == -1 else self.image_right,(self.x,self.y))
   ##Called on a schedule to check if fishy collides with any fish
   def check_collide(self,school):
 
@@ -88,8 +92,9 @@ class Fishy:
         self.collide(fish)
       
   def collide(self,other_fish):
-    if self.fish_eaten > other_fish.fish_eaten:
+    if self.fish_eaten >= other_fish.fish_eaten and other_fish.alive == True:
       other_fish.alive = False
+      self.fish_eaten += 1
     else:
       self.alive = False
 
@@ -97,31 +102,72 @@ class Fishy:
 
 class Fish():
   ##Init function
-  def __init__(self):
+  def __init__(self,width,height,x,y,direction,x_speed,fish_eaten):
     #positional variables
-    self.width = 40
-    self.height = 8 
-    self.x = 40
-    self.y = 40
-    self.direction:char = 'R'
-    self.x_speed = 0
-    self.y_speed = 0
+    self.width = width
+    self.height = height
+    self.x = x
+    self.y = y
+    self.direction = direction
+    self.x_speed = x_speed
+    #FIXME
     self.image_left = pygame.transform.scale(fishy_left_image,(self.width,self.height)) 
     self.image_right = pygame.transform.scale(fishy_right_image,(self.width,self.height)) 
     #stat variables
     self.alive = True
-    self.fish_eaten = 0
+    self.fish_eaten = fish_eaten
   
   def draw(self,screen):
     if self.alive:
-      screen.blit(self.image_left if self.direction == 'L' else self.image_right,(self.x,self.y))
+      screen.blit(self.image_left if self.direction == -1 else self.image_right,(self.x,self.y))
 
 
 
 class School():
   def __init__(self):
-    self.fish_list = [Fish()]
+    self.fish_list = []
   
+  def update(self):
+    #move all existing fish
+    #remove fish beyond boundaries 
+    #add new fish randomly
+    self.move()
+    self.check_clear_fish()
+    self.check_add_fish()
+
+    
+
+  def move(self):
+    for fish in self.fish_list:
+      fish.x += fish.x_speed
+      if fish.x + fish.width < 0 and fish.direction == -1:
+        fish.alive = False
+      elif fish.x > window_size[0] and fish.direction == 1:
+        fish.alive = False
+
+  def check_clear_fish(self):
+    for count,fish in enumerate(self.fish_list):
+      if not fish.alive:
+        self.fish_list.pop(count)
+
+  def check_add_fish(self):
+    #TODO add sizes to fish
+    if len(self.fish_list) < MAX_FISH:
+      #Generate fish
+      width = 40
+      height = 8
+      direction = 1 if random.randrange(0,1) else -1
+      x_speed = random.randrange(MIN_FISH_SPEED,MAX_FISH_SPEED) * direction
+      
+      x = -width if direction == 1 else window_size[0]
+      y = random.randrange(0,window_size[1]-height)
+
+      #TODO FIXME
+      fish_eaten=0
+      
+      new_fish = Fish(width,height,x,y,direction,x_speed,fish_eaten)
+      self.fish_list.append(new_fish)
+
   def draw(self,screen):
     for fish in self.fish_list:
       fish.draw(screen)
@@ -143,7 +189,7 @@ def main():
   
   #pygame.draw.rect(screen,(0,0,0),(225,200,40,20))
   clock = pygame.time.Clock()
-
+  
   main_fishy = Fishy()
   main_school = School()
   
@@ -151,14 +197,16 @@ def main():
   while running:
     #draw 
     screen.blit(fishy_background,(0,0))
-    clock.tick(30)
+    clock.tick(FPS)
     ##Initialize game start
-    all_fish = []
+
     ###MAIN GAME LOOP AFTER START
     while main_fishy.alive == True:
-      clock.tick(30)
+      clock.tick(FPS)
       #draw background
       screen.blit(fishy_background,(0,0))
+      #update fish_list
+      main_school.update()
       #handle main_fishy movement
       main_fishy.move()
       #check if fishy collided with any fish in the main_school
@@ -167,6 +215,7 @@ def main():
       main_school.draw(screen)
       #draw fishy on the screen
       main_fishy.draw(screen)
+      
       
       #TESTING PURPOSES
       main_fishy.fish_eaten = 1
@@ -179,6 +228,10 @@ def main():
         if event.type == pygame.QUIT:
           running = False
           pygame.quit()
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+        pygame.quit()
 
 
 
