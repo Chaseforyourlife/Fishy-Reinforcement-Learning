c735e86212ebb2214789acdaa10bf4757c795c7e
Chase Fashing <chasefashing@gmail.com>
2022-07-09 11:56:19 -0500

    adjusting parameters

--- game_ai.py
+++ game_ai.py
@@ -8,17 +8,24 @@ MAX_FISH = 1
 MAX_FISH_SPEED = 2 #6
 MIN_FISH_SPEED = 1 #2
 MAX_FISH_SIZE = 30 #150
+MIN_FISH_SIZE = 1 #-35
 
 BATCH_SIZE = 5000
-LEARNING_RATE = .1
+LEARNING_RATE = .0005
 MAX_MEMORY = 100_000
-EPSILON = 0
+EPSILON = 1
+MIN_EPSILON = .02
 GAMMA = 0.9 # must be less than 1
+
 INPUT_SIZE = 6+2+MAX_FISH*8
+
 HIDDEN_SIZE = 32
 HIDDEN2_SIZE = 16
 OUTPUT_SIZE = 9
 
+RANDOM_MOVE_INDEX = None
+RANDOM_MOVES_REMAINING =  0
+RANDOM_MOVES_CONSTANT = 30
 
 
 def calculate_reward(fishy,fish_eaten,win,flipped,stopped):
@@ -30,7 +37,7 @@ def calculate_reward(fishy,fish_eaten,win,flipped,stopped):
         reward -=15
         pass
     if fishy.alive:
-        reward -= 1
+        reward += 1
         pass
     else:
         reward -= 100
@@ -46,11 +53,14 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = EPSILON  # randomness
+        self.min_epsilon = MIN_EPSILON
         self.gamma = 0   # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft() when exceeding max_memory
         #TODO: model,trainer
         self.model = Linear_QNet(INPUT_SIZE,HIDDEN_SIZE,HIDDEN2_SIZE,OUTPUT_SIZE)
         self.trainer = QTrainer(self.model,LEARNING_RATE,GAMMA)
+        self.random_moves_remaining = 0
+        self.random_move_index = None
     def get_state(self,fishy,school):
         game_state = []
         #Input Layer Data
@@ -84,7 +94,8 @@ class Agent:
         self.trainer.train_step(state,action,reward,next_state,done)
 
     def train_long_memory(self):
-        self.epsilon -= .01
+        if self.epsilon > self.min_epsilon:
+            self.epsilon -= .01
         if len(self.memory) > BATCH_SIZE:
             mini_sample = random.sample(self.memory,BATCH_SIZE)
         else:
@@ -99,14 +110,19 @@ class Agent:
         #get empty list of 0s to replace with move
         move = [0,0,0,0,0,0,0,0,0]
         #Could change what goes into the randint max
-        if random.randrange(0,100)/100 < self.epsilon:
-            
+        if self.random_moves_remaining > 0:
+            move[self.random_move_index] = 1
+            self.random_moves_remaining -=1 
+        elif random.randrange(0,100)/100 < self.epsilon:
             move_index = random.randint(0,8)
             move[move_index] = 1
+            self.random_move_index = move_index
+            self.random_moves_remaining = RANDOM_MOVES_CONSTANT
         else:
             state_tensor = torch.tensor(state,dtype=torch.float)
             prediction = self.model(state_tensor)   # Calls Forward Function of LinearQModel
             move_index = torch.argmax(prediction).item()
+
             move[move_index] = 1
         return move
 
