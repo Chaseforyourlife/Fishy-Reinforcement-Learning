3d3a91cbd260ad9afa0d5cfd9c343b3b243e9c29
Chaseforyourlife <chasefashing@gmail.com>
2022-09-20 12:54:44 -0500

    create ai_states

---
+++ ai_states/model.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from variables import *
+
+import os
+
+TELEMETRY = False
+
+def printt(*strings):
+    if TELEMETRY:
+        for string in strings:
+            print(string)
+
+class Linear_QNet(nn.Module):
+    def __init__(self,input_size,hidden_size,hidden2_size,output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size,hidden_size)
+        self.linear2 = nn.Linear(hidden_size,hidden2_size)
+        #self.linear2 = nn.Linear(hidden_size,output_size)
+        self.linear3 = nn.Linear(hidden2_size,output_size)
+        self.load()
+    def forward(self,x):
+        x = F.relu(self.linear1(x))
+        #x = self.linear1(x)
+        x = F.relu(self.linear2(x))
+        #x = self.linear2(x)
+        x = self.linear3(x)
+        return x
+
+    def save(self,optimizer,file_name = 'model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.mkdir(model_folder_path)
+        file_name = os.path.join(model_folder_path,file_name)
+        torch.save({'model_state_dict':self.state_dict(),'optimizer_state_dict':optimizer.state_dict()},file_name)
+    def load(self,file_name = 'model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.mkdir(model_folder_path)
+        file_name = os.path.join(model_folder_path,file_name)
+        if os.path.exists(file_name):
+            checkpoint = torch.load(file_name)
+            self.load_state_dict(checkpoint['model_state_dict'])
+           
+
+class QTrainer:
+    def __init__(self,model,learning_rate,gamma):
+        self.model = model.to(DEVICE)
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.optimizer = optim.Adam(model.parameters(),lr=self.learning_rate)
+        self.criterion = nn.MSELoss()
+        self.load()
+
+    def load(self,file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.mkdir(model_folder_path)
+        file_name = os.path.join(model_folder_path,file_name)
+        if os.path.exists(file_name):
+            checkpoint = torch.load(file_name)
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    def train_step(self,state,action,reward,next_state,done):
+        
+        state = torch.tensor(state,dtype=torch.float)
+        next_state = torch.tensor(next_state,dtype=torch.float)
+        action = torch.tensor(action,dtype=torch.float)
+        reward = torch.tensor(reward,dtype=torch.float)
+
+        if len(state.shape)==1: # reshape tensors if there only one state is being trained on
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+        
+        ##Predicted Q values with the current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for index in range(len(done)):
+            printt(action[index])
+            printt(reward[index])
+            Q_new = reward[index]
+            #if not done[index]:
+                #printt('not done')
+                #Q_new = reward[index] + self.gamma * torch.max(self.model(next_state[index]))
+            Q_new = reward[index] + self.gamma * torch.max(self.model(next_state[index]))
+            target[index][torch.argmax(action[index]).item()] = Q_new
+        ## Q_new reward + gamma * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(target,pred)
+        #back propogation
+        loss.backward()
+
+        self.optimizer.step()
